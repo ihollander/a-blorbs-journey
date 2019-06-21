6bbbfd5ba7e4b446ff0212545ad53149100169a8
ihollander <ianholla@gmail.com>
2019-06-21 18:37:56 -0400

    oo refactor;

---
+++ src/scenes/MainGame.js
@@ -0,0 +1,163 @@
+import Phaser from "phaser";
+import sky from "../assets/sky.png";
+import ground from "../assets/platform.png";
+import star from "../assets/star.png";
+import bomb from "../assets/bomb.png";
+import dude from "../assets/dude.png";
+
+export default class MainGame extends Phaser.Scene {
+  constructor() {
+    super({ key: "MainGame" });
+  }
+
+  preload() {
+    this.load.image("sky", sky);
+    this.load.image("ground", ground);
+    this.load.image("star", star);
+    this.load.image("bomb", bomb);
+    this.load.spritesheet("dude", dude, {
+      frameWidth: 32,
+      frameHeight: 48
+    });
+  }
+
+  create() {
+    this.add.image(0, 0, "sky").setOrigin(0, 0);
+
+    // score
+    this.scoreText = this.add.text(16, 16, "Score: 0", {
+      fontSize: "32px",
+      fill: "#000"
+    });
+
+    // platforms
+    this.platforms = this.physics.add.staticGroup();
+
+    this.platforms
+      .create(400, 568, "ground")
+      .setScale(2)
+      .refreshBody();
+
+    this.platforms.create(600, 400, "ground");
+    this.platforms.create(50, 250, "ground");
+    this.platforms.create(750, 220, "ground");
+
+    // stars
+    this.stars = this.physics.add.group({
+      key: "star",
+      repeat: 11,
+      setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    this.stars.children.iterate(function(child) {
+      child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+
+    // player
+    this.player = this.physics.add.sprite(100, 450, "dude");
+
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    this.player.body.setGravityY(300);
+
+    // bomb
+    this.bombs = this.physics.add.group();
+
+    // collisions/physics
+    this.physics.add.collider(this.player, this.platforms);
+    this.physics.add.collider(this.stars, this.platforms);
+    this.physics.add.overlap(
+      this.player,
+      this.stars,
+      this.collectStar,
+      null,
+      this
+    );
+
+    this.physics.add.collider(this.bombs, this.platforms);
+    this.physics.add.collider(
+      this.player,
+      this.bombs,
+      this.hitBomb,
+      null,
+      this
+    );
+
+    // animations
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: "dude", frame: 4 }],
+      frameRate: 20
+    });
+
+    this.anims.create({
+      key: "right",
+      frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    if (this.cursors.left.isDown) {
+      this.player.setVelocityX(-160);
+
+      this.player.anims.play("left", true);
+    } else if (this.cursors.right.isDown) {
+      this.player.setVelocityX(160);
+
+      this.player.anims.play("right", true);
+    } else {
+      this.player.setVelocityX(0);
+
+      this.player.anims.play("turn");
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-530);
+    }
+  }
+
+  collectStar(player, star) {
+    star.disableBody(true, true);
+
+    this.score += 10;
+    this.scoreText.setText("Score: " + this.score);
+
+    if (this.stars.countActive(true) === 0) {
+      this.stars.children.iterate(function(child) {
+        child.enableBody(true, child.x, 0, true, true);
+      });
+
+      var x =
+        this.player.x < 400
+          ? Phaser.Math.Between(400, 800)
+          : Phaser.Math.Between(0, 400);
+
+      var bomb = bombs.create(x, 16, "bomb");
+      bomb.setBounce(1);
+      bomb.setCollideWorldBounds(true);
+      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    }
+  }
+
+  hitBomb(player, bomb) {
+    this.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play("turn");
+
+    this.gameOver = true;
+  }
+}
