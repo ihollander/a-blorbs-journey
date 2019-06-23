373c49a5be738049d15a8fb013e933814c6fb847
ihollander <ianholla@gmail.com>
2019-06-23 14:35:40 -0400

    some more sfx

--- src/consts/sounds.js
+++ src/consts/sounds.js
@@ -3,4 +3,8 @@ export const EXPLODE_SOUND = "EXPLODE_SOUND";
 export const KACHING_SOUND = "KACHING_SOUND";
 
 export const SPIT1_SOUND = "SPIT1_SOUND";
+export const SPIT2_SOUND = "SPIT2_SOUND";
 export const THUM2_SOUND = "THUM2_SOUND";
+
+export const WEOW_SOUND = "WEOW_SOUND";
+export const PEPEPEP_SOUND = "PEPEPEP_SOUND";
--- src/units/ChaserLarge.js
+++ src/units/ChaserLarge.js
@@ -1,4 +1,5 @@
 import { CHASER_IMAGE } from "../consts/images";
+import { WEOW_SOUND } from "../consts/sounds";
 
 import Enemy from "./Enemy";
 
@@ -9,9 +10,24 @@ export default class ChaserLarge extends Enemy {
     this.setScale(1, 1);
 
     this.health = 10;
+    this.sound = this.scene.sound.add(WEOW_SOUND, {
+      loop: true,
+      volume: 0
+    });
+    this.sound.play();
   }
 
   update() {
+    const distance = Phaser.Math.Distance.Between(
+      this.x,
+      this.y,
+      this.scene.player.x,
+      this.scene.player.y
+    );
+    if (distance < 1000) {
+      this.sound.setVolume((1000 - distance) / 1000);
+    }
+
     const angle = Phaser.Math.Angle.Between(
       this.x,
       this.y,
